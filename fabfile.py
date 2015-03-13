f3c95177f59a9d51451f987e0cabb210e9798710
alex <exa@snf-564156.vm.okeanos.grnet.gr>
2015-03-13 05:37:30 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -25,11 +25,12 @@ tables_options = {
 }
 
 def dbgen(sf=1, table=""):
-    table = tables_options[table]
+    if not table:
+       table = tables_options[table]
     chunks = len(env.hosts)
     if chunks > 0:
         chunk = env.hosts.index(env.host)
-
+    else: chunk = 0
     print "Scale Factor = ", sf
     print "Chunks = ", chunks
     print "Chunk = ", chunk
