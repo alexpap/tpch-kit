0c5b4756458332c86b6a2b7ecfb05d7a89ca42b5
alex <alpap@di.uoa.gr>
2015-03-13 08:15:30 +0200

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,34 +11,30 @@ tables_options = {
     'partsupp' : 'S'
 }
 
-def dbgen(sf=1, table=" "):
-    if not table:
-       table = "-T {tbl_opt}".format(tbl_opt=tables_options[table])
+def dbgen(sf, table):
+    # configure
+    table_opt=tables_options[table]
     chunks = len(env.hosts)
     if chunks > 0:
         chunk = env.hosts.index(env.host) + 1
-    else: chunk = 0
-    # print "Scale Factor = ", sf
-    # print "Chunks = ", chunks
-    # print "Chunk = ", chunk
-    # print "Table = ", table
+    else: chunk = 1
+    # generate
     with cd("~/tpch-kit/tpch/tpch_2_17_0/dbgen"):
-        dbgen_exec = "./dbgen -f -s {sf} -C {chunks} " \
-                     "-S {chunk} {tbl}".format(sf=sf, chunks=chunks, chunk=chunk, tbl=table)
-        # print dbgen_exec
+        dbgen_exec = "./dbgen -f -s {sf} -C {chunks} -S {chunk} " \
+                     "-T {tbl_opt}".format(sf=sf, chunks=chunks, chunk=chunk, tbl_opt=table_opt)
         run(dbgen_exec)
         run("mkdir -p ~/tpch-kit-datasets")
         run("for file in $(ls *.tbl*); "
             "do "
             "mv $(basename $file) ~/tpch-kit-datasets/;"
             "done")
+    # compress
     with cd("~/tpch-kit-datasets"):
-        run("for file in $(ls *.tbl); "
+        run("for file in $(ls *.tbl*); "
             "do "
             "mv $file ${file%.*};"
             "gzip ${file%.*};"
             "done")
-
 
 def list():
     with cd("~/tpch-kit-datasets"):
@@ -50,11 +46,11 @@ def clean():
 
 def install():
     with cd("~/"):
-        run("git clone https://github.com/alexpap/tpch-kit.git")
-
-def update():
-    with cd("~/tpch-kit"):
-        run("git pull")
+        run("if [ ! -d tpch-kit];"
+            "then git clone https://github.com/alexpap/tpch-kit.git;"
+            "else;"
+            "git pull;"
+            "fi;")
 
 def uninstall():
     with cd("~/"):
